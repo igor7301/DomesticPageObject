c642506238adb3dcfdf8e86b04168f2bba3353f6
igor7301 <codtQl1fkybgh>
2014-12-03 22:38:45 +0300

    Domestic  page object

---
+++ src/main/java/com/hotwire/AbstractPage.java
@@ -0,0 +1,38 @@
+package com.hotwire;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+
+
+/**
+ * Created by Admin on 07.11.14.
+ */
+public abstract class AbstractPage {
+    public static final int PAGE_WAIT = 2;
+
+
+    public AbstractPage(WebDriver webDriverInstance) {
+        PageFactory.initElements(webDriverInstance, this);
+    }
+
+    public AbstractPage(WebDriver webDriverInstance, By element) {
+        new WebDriverWait(webDriverInstance, PAGE_WAIT)
+                .until(ExpectedConditions.visibilityOfElementLocated(element));
+        PageFactory.initElements(webDriverInstance, this);
+    }
+
+    public AbstractPage(WebDriver webDriverInstance, String tile) {
+
+        //need to implement waiting for the page
+        assert (webDriverInstance.getPageSource().contains(tile));
+        PageFactory.initElements(webDriverInstance, this);
+
+
+    }
+
+}
---
+++ src/main/java/com/hotwire/HomePage.java
@@ -0,0 +1,60 @@
+package com.hotwire;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by Admin on 07.11.14.
+ */
+public class HomePage extends AbstractPage {
+
+    @FindBy(xpath = ".//*[@id='genRandom7-hotelDestinationFields']//input[@name='destCity']")
+    private WebElement hotelLocation;
+
+    @FindBy(xpath = ".//input[@id='startDateCalendarHotel-field']")
+    private WebElement startDate;
+
+    @FindBy(xpath = ".//input[@id='endDateCalendarHotel-field']")
+    private WebElement endDate;
+
+    @FindBy(xpath = ".//*[@id='findHotelButton']")
+    private WebElement startHotelSearch;
+
+
+    public HomePage(WebDriver webDriverInstance) {
+        super(webDriverInstance, "tile.hotwire.home");
+    }
+
+    public String getStartDate() {
+        return startDate.getAttribute("value");
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate.sendKeys(startDate);
+    }
+
+    public String getEndDate() {
+        return endDate.getAttribute("value");
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate.sendKeys(endDate);
+    }
+
+    public String getHotelLocation() {
+        return hotelLocation.getAttribute("value");
+    }
+
+    public void setHotelLocation(String hotelLocation) {
+        this.hotelLocation.sendKeys(hotelLocation);
+    }
+
+    public void startHotelSearch() {
+       startHotelSearch.click();
+    }
+
+}
